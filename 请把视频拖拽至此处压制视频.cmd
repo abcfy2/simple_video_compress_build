c459b7f0377be62bb216085330bffa0917c135cb
gerrit2 <info4@ok988.com>
2015-03-22 21:11:57 +0800

    修改windows批处理的缩放视频参数引号

--- 请把视频拖拽至此处压制视频.cmd
+++ 请把视频拖拽至此处压制视频.cmd
@@ -9,8 +9,8 @@ set FFMPEG="ffmpeg"
 set VIDEO_OPTS=-c:v libx264 -crf:v 24 -preset:v veryslow -x264opts me=umh:subme=7:no-fast-pskip:cqm=jvt 
 ::音频编码参数
 set AUDIO_OPTS=-c:a libfdk_aac -profile:a aac_he_v2 -vbr 2 
-::缩放视频
-::set SCALE_OPTS="-vf scale=-1:720" 
+::缩放视频(范例为缩放为720p的视频，-1代表自适应)
+::set SCALE_OPTS=-vf scale=-1:720 
 
 %FFMPEG% -y -i %input% %SCALE_OPTS% %VIDEO_OPTS% %AUDIO_OPTS% "%output%.mp4"
 
