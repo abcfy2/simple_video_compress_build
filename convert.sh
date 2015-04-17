79d29417edb639323aaf58af8843609e9b706afb
FengYu <yu.feng@kinglandiot.com>
2015-04-17 19:34:04 +0800

    修改linux脚本

--- convert.sh
+++ convert.sh
@@ -7,7 +7,14 @@ VIDEO_OPTS="-c:v libx264 -crf:v 24 -preset:v veryslow -x264opts me=umh:subme=7:n
 AUDIO_OPTS="-c:a libfdk_aac -profile:a aac_he_v2 -vbr 2" #音频编码参数
 #SCALE_OPTS="-vf scale=-1:720" #缩放视频
 
-for video in "$@"
+if [[ $# -eq 0 ]]; then
+    read -p "请输入要转码的视频路径,多个视频请用空格分割(可拖拽视频进入终端): " str
+    video_list=("${str[@]}")
+else
+    video_list=("$@")
+fi
+
+for video in "${video_list[@]}"
 do
     "$FFMPEG" -y -i "$video" $SCALE_OPTS $VIDEO_OPTS $AUDIO_OPTS "${video%.*}_enc.mp4"
 done
