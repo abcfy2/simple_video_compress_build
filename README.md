3b03e88ab8510b0cec1b0fd4b598d220f074ed9f
gerrit2 <info4@ok988.com>
2015-07-19 19:37:14 +0800

    修改推送地址，改为使用七牛

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 #simple_video_compress_build
 
-ffmpeg.exe文件就不上传了，构建的时候需要拷贝ffmpeg.exe到项目目录下，然后执行build.sh脚本自动推送到bae
+ffmpeg.exe文件就不上传了，构建的时候需要拷贝ffmpeg.exe到项目目录下，然后执行build.sh脚本自动推送到七牛云存储
+
+Build成品下载地址: [点此下载](http://7xjpgb.com1.z0.glb.clouddn.com/简易批量视频压制.7z)
--- build.sh
+++ build.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
-[[ -f config ]] && . config
-[[ -z "$sign_put_url" ]] && echo "未找到变量sign_put_url" && exit 1
+[[ -f config ]] && . config || exit 1
+
 rm -fr "简易批量视频压制"
 mkdir "简易批量视频压制"
 cp ./convert.sh ./readme.txt ./请把视频拖拽至此处压制视频.cmd ./ffmpeg.exe "简易批量视频压制"
 7za a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on 简易批量视频压制.7z 简易批量视频压制
 echo "上传中,请稍后..."
-curl -# -T 简易批量视频压制.7z -H "x-bs-acl: public-read" "$sign_put_url" -o /dev/null
+
+echo accesskey = $accesskey
+echo secretkey = $secretkey
+scope="$namespace:简易批量视频压制.7z"
+deadline=$((`date +%s` + 3600))
+putPolicy="{\"scope\":\"$scope\",\"deadline\":$deadline}"
+echo putPolicy = "$putPolicy"
+encodedPutPolicy=`echo -n "$putPolicy" | base64 -w0 | sed 's/\+/-/g; s/\//_/g'`
+echo encodedPutPolicy = "$encodedPutPolicy"
+sign=`echo -n "$encodedPutPolicy" | openssl sha1 -hmac "$secretkey" | awk '{print $2}' | xxd -r -p`
+encodedSign=`echo -n "$sign" | base64 -w0 | sed 's/\+/-/g; s/\//_/g'`
+echo encodedSign = $encodedSign
+uploadToken="$accesskey:$encodedSign:$encodedPutPolicy"
+echo uploadToken = $uploadToken
+
+curl -# -F token="$uploadToken" -F key='简易批量视频压制.7z' -F "file=@简易批量视频压制.7z; filename=简易批量视频压制.7z" http://upload.qiniu.com/ -o /dev/null
